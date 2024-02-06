a98479d29a08534c105ee8e1e2c6afecccf4df4e
Yan Amarante <yanlimaamarantec@hotmail.com>
2024-02-05 21:21:51 -0300

    feat: add list sales by filters params

--- dist/sales/controllers.js
+++ dist/sales/controllers.js
@@ -6,51 +6,88 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createSale = exports.listSales = void 0;
 const database_1 = __importDefault(require("../database"));
 const queries_1 = require("./queries");
-async function listSales(req, res) {
-    const queryParams = req.query.page;
-    let values = [(queryParams - 1) * 14];
-    if (queryParams === undefined) {
-        database_1.default.query(queries_1.listSalesQueries).then((resultado) => {
-            const transformedObject = resultado.rows.reduce((acc, item) => {
-                acc[item.id] = {
-                    id: item.id,
-                    image: item.image,
-                    name: item.name,
-                    pattern: item.pattern,
-                    wear: item.wear,
-                    price: item.price,
-                    category: item.category
-                };
-                return acc;
-            }, {});
-            res.status(200).send(transformedObject);
-        }, (erro) => {
-            res.status(500).send({ erro: erro });
-        });
+function transformToObject(array) {
+    const object = array.reduce((acc, item) => {
+        acc[item.id] = {
+            id: item.id,
+            image: item.image,
+            name: item.name,
+            pattern: item.pattern,
+            wear: item.wear,
+            price: item.price,
+            category: item.category
+        };
+        return acc;
+    }, {});
+    return object;
+}
+async function listAllSales(req, res) {
+    try {
+        let result = await database_1.default.query(queries_1.listSalesQueries);
+        const transformedObject = transformToObject(result.rows);
+        res.status(200).send(transformedObject);
     }
-    else if (queryParams !== undefined) {
-        const resultCount = await database_1.default.query(queries_1.listSalesCount);
-        const totalItems = resultCount.rows[0].count;
+    catch (erro) {
+        res.status(500).send({ erro: erro });
+    }
+}
+async function countNumberOfPages(query, value = null) {
+    let resultCount;
+    if (value === null)
+        resultCount = await database_1.default.query(query);
+    else
+        resultCount = await database_1.default.query(query, value);
+    const totalItems = resultCount.rows[0].count;
+    const limit = 14;
+    const numberOfPages = Math.ceil(totalItems / limit);
+    if (numberOfPages === 0)
+        return 1;
+    else
+        return numberOfPages;
+}
+async function listSales(req, res) {
+    const pageParams = req.query.page;
+    const filterParams = [req.query.name, req.query.wear];
+    const nameParams = [req.query.name];
+    const wearParams = [req.query.wear];
+    if (pageParams === undefined) {
+        listAllSales(req, res);
+    }
+    else if (pageParams !== undefined) {
         const limit = 14;
-        const rowsNumber = Math.ceil(totalItems / limit);
-        const result = await database_1.default.query(queries_1.listSalesPageQueries, values);
-        const transformedObject = result.rows.reduce((acc, item) => {
-            acc[item.id] = {
-                id: item.id,
-                image: item.image,
-                name: item.name,
-                pattern: item.pattern,
-                wear: item.wear,
-                price: item.price,
-                category: item.category
-            };
-            return acc;
-        }, {});
+        const values = [(pageParams - 1) * limit];
+        let rowsNumber;
+        let result;
+        if (nameParams[0] && wearParams[0] !== undefined)
+            rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales WHERE name=$1 AND wear=$2", filterParams);
+        else if (nameParams[0] !== undefined && wearParams[0] === undefined)
+            rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales WHERE name=$1", nameParams);
+        else if (nameParams[0] === undefined && wearParams[0] !== undefined)
+            rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales WHERE wear=$1", wearParams);
+        else if (nameParams[0] && wearParams[0] === undefined)
+            rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales");
+        if (nameParams[0] && wearParams[0] !== undefined) {
+            values.push(nameParams[0], wearParams[0]);
+            result = await database_1.default.query('SELECT * FROM sales WHERE name=$2 AND wear=$3 OFFSET $1 LIMIT 14', values);
+        }
+        else if (nameParams[0] !== undefined && wearParams[0] === undefined) {
+            values.push(nameParams[0]);
+            result = await database_1.default.query('SELECT * FROM sales WHERE name=$2 OFFSET $1 LIMIT 14', values);
+        }
+        else if (nameParams[0] === undefined && wearParams[0] !== undefined) {
+            values.push(wearParams[0]);
+            result = await database_1.default.query('SELECT * FROM sales WHERE wear=$2 OFFSET $1 LIMIT 14', values);
+        }
+        else {
+            result = await database_1.default.query('SELECT * FROM sales OFFSET $1 LIMIT 14', values);
+        }
+        const transformedObject = transformToObject(result.rows);
         transformedObject.numberOfPages = rowsNumber;
         res.status(200).send(transformedObject);
     }
 }
 exports.listSales = listSales;
+//rowsNumber by filter // search by query params
 async function createSale(req, res) {
     const values = [req.body.image, req.body.name, req.body.pattern, req.body.wear, req.body.price, req.body.category];
     database_1.default.query(queries_1.createSaleQueries, values).then((resultado) => {
--- src/sales/controllers.ts
+++ src/sales/controllers.ts
@@ -1,80 +1,147 @@
 import database from "../database";
 
-import { listSalesQueries, listSalesPageQueries, listSalesCount, createSaleQueries } from "./queries"
+import { listSalesQueries, listSalesCount, listSalesPageQueries, createSaleQueries } from "./queries"
 
 import { Request, Response } from "express";
 
 
+function transformToObject(array: any) {
+
+    const object = array.reduce((acc: any, item: any) => {
+        acc[item.id] = {
+            id: item.id,
+            image: item.image,
+            name: item.name,
+            pattern: item.pattern,
+            wear: item.wear,
+            price: item.price,
+            category: item.category
+        };
+        return acc;
+    }, {})
+
+    return object
+
+}
+
+
+async function listAllSales(req: Request, res: Response) {
+
+
+    try {
+
+        let result: any = await database.query(listSalesQueries)
+
+        const transformedObject = transformToObject(result.rows)
+
+        res.status(200).send(transformedObject)
+
+    }
+
+    catch (erro) {
+
+        res.status(500).send({ erro: erro })
+
+    }
+
+}
+
+async function countNumberOfPages(query: string, value: any = null) {
+
+
+    let resultCount: any
+
+    if (value === null) resultCount = await database.query(query)
+
+    else resultCount = await database.query(query, value)
+
+    const totalItems = resultCount.rows[0].count
+
+    const limit = 14
+
+    const numberOfPages = Math.ceil(totalItems / limit)
+    
+    if (numberOfPages === 0) return 1
+
+    else return numberOfPages
+
+}
+
 async function listSales(req: Request, res: Response) {
 
-    const queryParams: any = req.query.page
+    const pageParams: any = req.query.page
 
-    let values = [(queryParams - 1) * 14]
+    const filterParams = [req.query.name, req.query.wear]
 
-    if (queryParams === undefined) {
+    const nameParams: any = [req.query.name]
 
-        database.query(listSalesQueries).then(
+    const wearParams: any = [req.query.wear]
 
-            (resultado) => {
 
-                const transformedObject = resultado.rows.reduce((acc, item) => {
-                    acc[item.id] = {
-                        id: item.id,
-                        image: item.image,
-                        name: item.name,
-                        pattern: item.pattern,
-                        wear: item.wear,
-                        price: item.price,
-                        category: item.category
-                    };
-                    return acc;
-                }, {})
+    if (pageParams === undefined) {
 
-                res.status(200).send(transformedObject)
+        listAllSales(req, res)
 
-            },
-
-            (erro) => {
-
-                res.status(500).send({ erro: erro })
-
-            }
-
-        )
-
-    } else if (queryParams !== undefined) {
-
-        const resultCount = await database.query(listSalesCount)
-
-        const totalItems = resultCount.rows[0].count
+    } else if (pageParams !== undefined) {
 
         const limit = 14
 
-        const rowsNumber = Math.ceil(totalItems / limit)
+        const values = [(pageParams - 1) * limit]
 
-        const result = await database.query(listSalesPageQueries, values)
+        let rowsNumber
 
-        const transformedObject = result.rows.reduce((acc, item) => {
-            acc[item.id] = {
-                id: item.id,
-                image: item.image,
-                name: item.name,
-                pattern: item.pattern,
-                wear: item.wear,
-                price: item.price,
-                category: item.category
-            };
-            return acc;
-        }, {})
+        let result: any
+
+
+        if (nameParams[0] && wearParams[0] !== undefined) rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales WHERE name=$1 AND wear=$2", filterParams)
+
+        else if (nameParams[0] !== undefined && wearParams[0] === undefined) rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales WHERE name=$1", nameParams)
+
+        else if (nameParams[0] === undefined && wearParams[0] !== undefined) rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales WHERE wear=$1", wearParams)
+
+        else if (nameParams[0] && wearParams[0] === undefined) rowsNumber = await countNumberOfPages("SELECT count(*) FROM sales")
+
+        if (nameParams[0] && wearParams[0] !== undefined) {
+
+            values.push(nameParams[0], wearParams[0])
+
+            result = await database.query('SELECT * FROM sales WHERE name=$2 AND wear=$3 OFFSET $1 LIMIT 14', values)
+
+        }
+        else if (nameParams[0] !== undefined && wearParams[0] === undefined) {
+
+            values.push(nameParams[0])
+
+            result = await database.query('SELECT * FROM sales WHERE name=$2 OFFSET $1 LIMIT 14', values)
+
+        }
+
+        else if (nameParams[0] === undefined && wearParams[0] !== undefined) {
+
+            values.push(wearParams[0])
+
+            result = await database.query('SELECT * FROM sales WHERE wear=$2 OFFSET $1 LIMIT 14', values)
+
+        }
+
+        else {
+
+            result = await database.query('SELECT * FROM sales OFFSET $1 LIMIT 14', values)
+
+        }
+
+
+        const transformedObject = transformToObject(result.rows)
 
         transformedObject.numberOfPages = rowsNumber
+
 
         res.status(200).send(transformedObject)
 
     }
 
 }
-
+//rowsNumber by filter // search by query params
 async function createSale(req: Request, res: Response) {
 
     const values = [req.body.image, req.body.name, req.body.pattern, req.body.wear, req.body.price, req.body.category]
